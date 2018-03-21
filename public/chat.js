db75df39168abe7c3859a1d4e2c39b85c8b2255f
Nader M. Nassar <nnassar@us.ibm.com>
2018-03-21 03:57:17 -0400

    port80

--- public/chat.js
+++ public/chat.js
@@ -2,7 +2,7 @@ window.onload = function() {
  
     var messages = [];
    // var socket = io.connect('https://lab3chat-pace-cit322.c9users.io:8080');
- var socket = io.connect('https://cit233lab3.herokuapp.com:8080');   
+ var socket = io.connect('https://cit233lab3.herokuapp.com');   
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
