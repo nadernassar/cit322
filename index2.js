077d24f413fefd9667e3b36e838437bd1ff1526c
Nader M. Nassar <nnassar@us.ibm.com>
2018-03-23 00:22:34 -0400

    var port

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 3000;
+var PORT = process.env.PORT || 5000;
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
