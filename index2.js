77f76d6936d2c2858c1f2e8750753313ed34b82a
Nader M. Nassar <nnassar@us.ibm.com>
2018-03-23 00:24:45 -0400

    PORT

--- index2.js
+++ index2.js
@@ -5,8 +5,8 @@ app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'));
-var io = require('socket.io').listen(app.listen(port));
-console.log("Listening on port " + port);
+var io = require('socket.io').listen(app.listen(PORT));
+console.log("Listening on port " + PORT);
 
 app.get("/", function(req, res){
 
